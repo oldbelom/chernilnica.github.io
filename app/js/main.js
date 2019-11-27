d4dd2145815b3c51833de06007d229c86bbcd4b5
oldbelom <oldbelom@gmail.com>
2019-11-27 23:45:57 +0300

    some fixes

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//   //Для липкого меню
+//   $(window).scroll(function() {
+//     if ($(this).scrollTop() >= 90) {
+//       $(".header__bottom").addClass("fix");
+//     } else {
+//       $(".header__bottom").removeClass("fix");
+//     }
+//   });
+//   //конец липкого меню
+// });
